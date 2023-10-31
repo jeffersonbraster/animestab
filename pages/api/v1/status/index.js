f439d4cff7cc5914d73ac30a5b573c9b6cf40343
Jefferson <jefferson.brandao@g.globo>
2023-10-30 23:32:49 -0300

    configurando banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-  res.status(200).json({ status: 'ok' })
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows[0].sum);
+  res.status(200).json({ status: "ok" });
 }
 
-export default status
+export default status;
