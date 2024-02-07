c894d199d34b679e191906d4d1bf7d03a3ffab5d
Jefferson <jefferson.brandao@g.globo>
2024-02-06 22:58:29 -0300

    criando a rota status do banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows[0].sum);
-  res.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+
+  const databaseMaxConnectResult = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult.rows[0].server_version,
+        max_connections: parseInt(databaseMaxConnectResult.rows[0].max_connections),
+        opened_connections: databaseOpenedConnectionsResult.rows[0].count, 
+      },
+    },
+  });
 }
 
 export default status;
